dcdce3a66a1fd7130f53151f1c6c129438d7cc8c
Mark Sweet <marks828@gmail.com>
2016-10-13 12:22:27 -0400

    index and show for houses and characters

--- game_of_rails/app/controllers/houses_controller.rb
+++ game_of_rails/app/controllers/houses_controller.rb
@@ -8,4 +8,20 @@ class HousesController < ApplicationController
     @house = House.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
 end
