2781451043e7bc817748af99b2b2aab4130c6272
Mark Sweet <marks828@gmail.com>
2016-10-13 16:08:17 -0400

    full crud functionality

--- game_of_rails/app/controllers/characters_controller.rb
+++ game_of_rails/app/controllers/characters_controller.rb
@@ -12,15 +12,38 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
-  def edit
-
-  end
-
   def create
+    @character = Character.new(characters_params)
+    @character.house_id = params[:character][:house].to_i
+    @character.save
 
+    redirect_to characters_path
   end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
 
   def update
+    @character = Character.find(params[:id])
+    @character.update(characters_params)
+
+    redirect_to characters_path
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to characters_path
+  end
+
+
+
+  private
+  def characters_params
+    params.require(:character).permit(:name, :img_url, :alive, :bastard, :fireproof)
 
   end
 
--- game_of_rails/app/controllers/houses_controller.rb
+++ game_of_rails/app/controllers/houses_controller.rb
@@ -13,15 +13,33 @@ class HousesController < ApplicationController
   end
 
   def create
+    @house = House.create(houses_params)
+    @house.save
 
+    redirect_to houses_path
   end
 
   def edit
-
+    @house = House.find(params[:id])
   end
 
   def update
+    @house = House.find(params[:id])
+    @house.update(houses_params)
 
+    redirect_to houses_path
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+
+    redirect_to houses_path
+  end
+
+  private
+  def houses_params
+    params.require(:house).permit(:name, :img_url, :motto, :territory)
   end
 
 end
